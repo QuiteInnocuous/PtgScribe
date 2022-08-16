dd5a82a670397aeaf56d315cb4573a1fa0f85b31
J Raymond Davidson <111265147+QuiteInnocuous@users.noreply.github.com>
2022-08-15 22:54:57 -0700

    Building and showing preview

--- app/src/main/java/com/quiteinnocuous/ptgscribe/MainActivity.kt
+++ app/src/main/java/com/quiteinnocuous/ptgscribe/MainActivity.kt
@@ -3,26 +3,34 @@ package com.quiteinnocuous.ptgscribe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.quiteinnocuous.ptgscribe.ui.theme.PathToGloryScribeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
+
             PathToGloryScribeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Scaffold {
+                    paddingValues ->
+                    NavigationComponent(
+                        navController,
+                        modifier = Modifier.padding(paddingValues = paddingValues)
+                    )
                 }
             }
         }
@@ -30,14 +38,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun NavigationComponent(
+    navController: NavHostController,
+    modifier: Modifier
+) {
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = "home"
+    ) {
+        composable("home") {
+            HomeScreen(navController)
+        }
+        composable("createArmy") {
+            CreateArmyScreen()
+        }
+    }
+}
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    Button(onClick = { navController.navigate("createArmy") }) {
+        Text(stringResource(R.string.create_army))
+    }
+}
+
+@Composable
+fun CreateArmyScreen() {
+    Text(stringResource(R.string.create_army))
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     PathToGloryScribeTheme {
-        Greeting("Android")
+        HomeScreen(rememberNavController())
     }
 }
